fc9423ed810c8a105b5f1d598e73eb51826c4afa
Serhii Yolkin <fearcatalyst@gmail.com>
2017-04-08 14:33:58 +0200

    Moved SimpleXmlSerializer into a submodule

--- LostPolygon.Common.SimpleXmlSerialization/SerializationMethodAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace LostPolygon.Common.SimpleXmlSerialization {
-    [AttributeUsage(AttributeTargets.Method)]
-    public class SerializationMethodAttribute : Attribute {
-    }
-}
